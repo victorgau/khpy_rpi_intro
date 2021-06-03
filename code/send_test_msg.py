79a9cf89f397662cacecfab37bd4559d2bdc06bb
Victor Gau <victorgau@gmail.com>
2021-06-03 19:40:43 +0800

    Update send_test_msg.py

--- code/send_test_msg.py
+++ code/send_test_msg.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 import time
 import random
 
-n = 1
+i = random.randint(1, 10000)
 
 # broker = "iot.eclipse.org" 
 broker = "broker.hivemq.com"
@@ -13,10 +13,9 @@ client = mqtt.Client("client_"+str(random.random()))
 client.connect(broker)
 
 while True:
-    for i in range(n):
-        T = str(random.randint(25,30))
-        H = str(random.randint(50,70))
-        print(f"{i} sending ==> 溫度：{T}, 濕度：{H}")
-        client.publish(f"wetland/sensor/{i}/溫度", T)
-        client.publish(f"wetland/sensor/{i}/濕度", H)
+    T = str(random.randint(25,30))
+    H = str(random.randint(50,70))
+    print(f"{i} sending ==> 溫度：{T}, 濕度：{H}")
+    client.publish(f"wetland/sensor/{i}/溫度", T)
+    client.publish(f"wetland/sensor/{i}/濕度", H)
     time.sleep(5)
